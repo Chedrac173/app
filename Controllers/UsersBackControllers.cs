a9b1f5cdd25a4d976d88619e3712fc5006511255
Chedrac173 <89912372+Chedrac173@users.noreply.github.com>
2022-02-14 19:48:43 +0000

    Add files via upload

---
+++ Controllers/UsersBackControllers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaApirefais.Data;
+using CinemaApirefais.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CinemaApirefais.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+
+        public MoviesController(CinemaDbContext dbContext) 
+        {
+            _dbContext = dbContext; 
+        }
+
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return _dbContext.Movies;  
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MoviesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
